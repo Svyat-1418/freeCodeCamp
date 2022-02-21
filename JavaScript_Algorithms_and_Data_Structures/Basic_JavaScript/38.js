85b60b4bcc2fc48294b2288b034c42b214f2bbaa
Svyat-1418 <68824715+Svyat-1418@users.noreply.github.com>
2022-02-21 14:57:16 +0300

    38:Word Blanks

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/38.js
@@ -0,0 +1,8 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "We " + myVerb + " with our " + myAdjective + " " + myNoun + " " + myAdverb; // Change this line
+// Only change code above this line
